99ef5dc029a0e7d15c82d420104416058cdd30b3
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-04-24 02:57:58 +0300

    Следование камеры за дрифтом

--- [gameplay]/dpCameraViews/client/CockpitView.lua
+++ [gameplay]/dpCameraViews/client/CockpitView.lua
@@ -2,6 +2,18 @@ CockpitView = {}
 local isActive = false
 local positionOffset = Vector3()
 local lookOffset = Vector3()
+local previousPosition = Vector3()
+
+local function differenceBetweenAngles(firstAngle, secondAngle)
+	local difference = secondAngle - firstAngle
+	while difference < -180 do
+		difference = difference + 360
+	end
+	while difference > 180 do
+		difference = difference - 360
+	end
+	return difference
+end
 
 local function wrapAngle(value)
 	if not value then
@@ -21,10 +33,15 @@ local function update(deltaTime)
 	end
 	deltaTime = deltaTime / 1000
 	
+	-- Положение камеры в машине
 	local cameraPos = localPlayer.vehicle.matrix:transformPosition(positionOffset)
 	local cameraLook = localPlayer.vehicle.matrix:transformPosition(lookOffset)
 	local cameraRoll = -localPlayer.vehicle.rotation.y
 
+	-- Следование камеры за дрифтом
+	cameraLook = cameraLook + localPlayer.vehicle.velocity
+	
+	-- Обновление камеры
 	Camera.setMatrix(cameraPos, cameraLook, cameraRoll)
 end
 

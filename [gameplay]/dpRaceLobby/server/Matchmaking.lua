85a2d043b0f64ec32690b3fa218af7c6695b8dd0
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-10-16 15:27:33 +0300

    Гонки
    - Переписан поиск игроков
    - Игроки без дрифт настроек больше не попадают в дрифт-гонки

--- [gameplay]/dpRaceLobby/server/Matchmaking.lua
+++ [gameplay]/dpRaceLobby/server/Matchmaking.lua
@@ -1,7 +1,138 @@
 Matchmaking = {}
+local ROOM_TIME_NOPLAYERS = 20
+local ROOM_TIME_ENOUGH_PLAYERS = 10
+local rooms = {}
+local players = {}
 
-local playersInSearch = {}
-local playersCheckTimer
+local function findRoom(rank, driftAllowed)
+	for i, room in ipairs(rooms) do
+		if room.rank == rank and (room.driftAllowed == driftAllowed or not room.driftAllowed) then
+			return room
+		end
+	end
+	return false
+end
+
+local function createRoom(rank, driftAllowed)
+	local room = {
+		rank = rank,
+		driftAllowed = driftAllowed,
+		time = ROOM_TIME_NOPLAYERS,
+		players = {}
+	}
+	table.insert(rooms, room)
+	return room
+end
+
+local function removeRoom(room)
+	if not room then
+		return
+	end
+	for i, r in ipairs(rooms) do
+		if r == room then
+			table.remove(rooms, i)
+			return true
+		end
+	end
+	return false
+end
+
+local function getRoomPlayers(room)
+	if not room then
+		return false
+	end
+	local players = {}
+	for player in pairs(room.players) do
+		table.insert(players, player)
+	end
+	return players
+end
+
+local function updateRoom(room)
+	if not room then
+		return
+	end
+	room.time = room.time - 1
+	--outputDebugString("Room time: " .. tostring(room.time))	
+	--outputDebugString("Players count: " .. tostring(#getRoomPlayers(room)))
+	if room.time <= 0 then		
+		local playersList = getRoomPlayers(room)
+		if #playersList < GlobalConfig.MATCHMAKING_MIN_PLAYERS then
+			room.time = ROOM_TIME_NOPLAYERS
+		else
+			for i, p in ipairs(playersList) do
+				Matchmaking.removePlayer(p)
+			end
+			RaceManager.raceReady(playersList)
+			removeRoom(room)
+		end
+	end
+end
+
+function Matchmaking.addPlayer(player)
+	if not isElement(player) then
+		return false
+	end
+	if not player.vehicle then
+		return false
+	end
+	if players[player] then
+		return false
+	end
+	local vehicleOwner = player.vehicle:getData("owner_id")
+	local playerId = player:getData("_id")
+	if not vehicleOwner or not playerId or vehicleOwner ~= playerId then
+		return false
+	end
+	local rank = exports.dpShared:getVehicleClass(player.vehicle.model)
+	local driftAllowed = player.vehicle:getData("DriftHandling") > 0
+	local room = findRoom(rank, driftAllowed)
+	if not room then
+		room = createRoom(rank, driftAllowed)
+	end
+	room.players[player] = true
+	players[player] = room
+	local roomPlayers = getRoomPlayers(room)
+	if #roomPlayers >= GlobalConfig.MATCHMAKING_MIN_PLAYERS and #roomPlayers < GlobalConfig.MATCHMAKING_MAX_PLAYERS then
+		if room.time > ROOM_TIME_ENOUGH_PLAYERS then
+			room.time = ROOM_TIME_ENOUGH_PLAYERS
+		end
+	elseif #roomPlayers >= GlobalConfig.MATCHMAKING_MAX_PLAYERS then
+		room.time = 0
+		updateRoom(room)
+	end
+	return true
+end
+
+function Matchmaking.removePlayer(player)
+	if not player then
+		return false
+	end
+	if not players[player] then
+		return false
+	end
+
+	local room = players[player]
+	if room then
+		room.players[player] = nil
+	end
+	players[player] = nil
+	local roomPlayers = getRoomPlayers(room)
+	if #roomPlayers < GlobalConfig.MATCHMAKING_MIN_PLAYERS and #roomPlayers > 0 then
+		room.time = ROOM_TIME_NOPLAYERS
+	elseif #roomPlayers <= 0 then
+		removeRoom(room)
+	end
+	return true
+end
+
+setTimer(function ()
+	for i, room in ipairs(rooms) do
+		updateRoom(room)
+	end
+
+	--outputDebugString("Rooms count: " .. tostring(#rooms))
+end, 1000, 0)
 
 local addClientEventHandler = function(event, handler) 
 	addEvent(event, true)
@@ -16,79 +147,6 @@ addClientEventHandler("dpRaceLobby.cancelSearch", function ()
 	Matchmaking.removePlayer(client)
 end)
 
-local function delayPlayersCheck()
-	if isTimer(playersCheckTimer) then
-		return
-	end
-	playersCheckTimer = setTimer(Matchmaking.checkPlayers, GlobalConfig.MATCHMAKING_GAME_CHECK_INTERVAL, 1)
-end
-
-function Matchmaking.addPlayer(player)
-	if not isElement(player) then
-		return false
-	end
-	if playersInSearch[player] then
-		return false
-	end
-	if not player.vehicle then
-		return false
-	end
-	local vehicleOwner = player.vehicle:getData("owner_id")
-	local playerId = player:getData("_id")
-	if not vehicleOwner or not playerId or vehicleOwner ~= playerId then
-		return false
-	end
-	local vehicleClass = exports.dpShared:getVehicleClass(player.vehicle.model)
-
-	playersInSearch[player] = {
-		class = vehicleClass,
-		addedAt = getRealTime().timestamp
-	}
-	delayPlayersCheck()
-	return true
-end
-
-function Matchmaking.removePlayer(player)
-	if not player then
-		return false
-	end
-	if not playersInSearch[player] then
-		return false
-	end
-
-	playersInSearch[player] = nil
-
-	return true
-end
-
-function Matchmaking.checkPlayers()
-	local playersByClass = {{}, {}, {}, {}, {}}
-
-	local playersCount = 0
-	for player, info in pairs(playersInSearch) do
-		if not playersByClass[info.class] then
-			playersByClass[info.class] = {}
-		end
-		table.insert(playersByClass[info.class], player)
-		playersCount = playersCount + 1
-	end
-	root:setData("MatchmakingSearchingPlayersCount", playersCount)
-
-	for class, players in ipairs(playersByClass) do
-		if #players >= GlobalConfig.MATCHMAKING_MIN_PLAYERS then
-			table.sort(players, function(player1, player2)
-				return player1:getData("xp") < player2:getData("xp")
-			end)	
-
-			local count = math.min(#players, GlobalConfig.MATCHMAKING_MAX_PLAYERS)
-			local matchPlayers = {}
-			outputDebugString("Matchmaking: creating game (player count: " .. tostring(count) .. ")")
-			for i = 1, count do 
-				local player = players[i]
-				Matchmaking.removePlayer(player)
-				table.insert(matchPlayers, player)
-			end
-			RaceManager.raceReady(matchPlayers)
-		end
-	end
-end
+addEventHandler("onPlayerQuit", root, function ()
+	Matchmaking.removePlayer(source)
+end)
--- [gameplay]/dpRaceLobby/shared/GlobalConfig.lua
+++ [gameplay]/dpRaceLobby/shared/GlobalConfig.lua
@@ -2,8 +2,6 @@ GlobalConfig = {}
 
 -- Время, которое даётся игрокам на подтверждения участия в гонке
 GlobalConfig.PENDING_RACE_TIMEOUT = 20000
--- Интервал, по которому проверяется, может ли быть создана игра
-GlobalConfig.MATCHMAKING_GAME_CHECK_INTERVAL = 3000
 -- Минимальное число участников гонки
 GlobalConfig.MATCHMAKING_MIN_PLAYERS = 2
 -- Максимальное число участников гонки

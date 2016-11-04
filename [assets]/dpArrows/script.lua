56a0560f7c9bec1d0f23a3f32c6c62d813bd35cf
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-11-04 21:28:27 +0300

    Исправлены стрелки в гонках

--- [assets]/dpArrows/script.lua
+++ [assets]/dpArrows/script.lua
@@ -1,62 +1,82 @@
+local MODEL = 3851
+
 local UVSpeed = {-0.5, 0}
 local UVResize = {1, 1}
 local pSpeed = 0.5 -- pendulum speed - must be a multiplication of 0.25 value ( set 0 - to turn off )
 local pMinBright = 0.35 -- minimum brightness ( 0 - 1)
 
-addEventHandler("onClientResourceStart",resourceRoot,
-	function()
-		local shader = dxCreateShader("shader.fx")
+local walls = {}
 
-		if not shader then
-			return outputChatBox( "Could not create shader. Please use debugscript 3" )
-		end
-		
-		local texture = dxCreateTexture("arrow.png","dxt5")
-		dxSetShaderValue(shader,"Tex",texture)
-		
-		dxSetShaderValue(shader,"UVSpeed", UVSpeed)
-		dxSetShaderValue(shader,"UVResize", UVResize)
-		dxSetShaderValue(shader,"pSpeed", pSpeed)
-		dxSetShaderValue(shader,"pMinBright", pMinBright)
+function showArrow(object)
+    if not isElement(object) then
+        return
+    end
+    if object.model ~= model then
+        return
+    end
 
-		engineApplyShaderToWorldTexture(shader,"ws_carshowwin1")
-		engineSetModelLODDistance (3851, 300)
-		engineSetModelLODDistance (18000, 1000)
-	end
-)
+    setObjectBreakable(object, false)
+    setElementDoubleSided(object, true)
 
-addEventHandler("onClientResourceStart", root,
-    function()
-		local objects = getElementsByType("object")
-		for k, object in ipairs(objects) do 
-			if getElementModel(object) == 3851 then
-			setObjectBreakable(object, false)
-			setElementDoubleSided(object, true)
-			local x, y, z = getElementPosition(object)
-			local rx, ry, rz = getElementRotation(object)
-			newObj = createObject(18000, x, y, z, rx, ry, rz)
-			setElementAlpha(newObj, 0)
-			setElementParent(newObj, object)
-			end
+    local x, y, z = getElementPosition(object)
+    local rx, ry, rz = getElementRotation(object)
+    walls[object] = createObject(18000, x, y, z, rx, ry, rz)
+    setElementAlpha(walls[object], 0)
+    setElementParent(walls[object], object)
+end
+
+function hideArrow(object)
+    if not isElement(object) then
+        return
+    end
+    if object.model ~= MODEL then
+        return
+    end
+
+    if isElement(walls[object]) then
+        destroyElement(walls[object])
+    end
+end
+
+addEventHandler("onClientElementStreamIn", root, function ()
+    showArrow(source)
+end)
+
+addEventHandler("onClientElementStreamOut", root, function ()
+    hideArrow(source)
+end)
+
+addEventHandler("onClientElementDestroy", root, function ()
+    hideArrow(source)
+end)
+
+addEventHandler("onClientResourceStart", resourceRoot, function ()
+    local col = engineLoadCOL("1.col")
+    engineReplaceCOL(col, 18000)
+    local dff = engineLoadDFF("1.dff")
+    engineReplaceModel(dff, 18000) 
+
+    local shader = dxCreateShader("shader.fx")
+    local texture = dxCreateTexture("arrow.png","dxt5")
+
+    dxSetShaderValue(shader,"Tex",texture)
+    dxSetShaderValue(shader,"UVSpeed", UVSpeed)
+    dxSetShaderValue(shader,"UVResize", UVResize)
+    dxSetShaderValue(shader,"pSpeed", pSpeed)
+    dxSetShaderValue(shader,"pMinBright", pMinBright)
+
+    engineApplyShaderToWorldTexture(shader,"ws_carshowwin1")
+    engineSetModelLODDistance(MODEL, 300)
+
+    for i, object in ipairs(getElementsByType("object")) do
+        if object.model == MODEL and isElementStreamedIn(object) then
+            showArrow(object)
         end
     end
-)
+end)
 
-addEventHandler("onClientResourceStart", resourceRoot,
-    function()
-		col = engineLoadCOL ( "1.col" )
-		engineReplaceCOL ( col, 18000 )
-		dff = engineLoadDFF ( "1.dff" )
-		engineReplaceModel ( dff, 18000 )
-		outputConsole("Loading NFS-Style Arrows by AleksCore & Ren712")
-		outputConsole("---------------------->[Arrow ID: #ff00003851]")
+addEventHandler("onClientObjectBreak", root, function()
+    if getElementModel(source) == MODEL then
+        cancelEvent()
     end
-)
-
-addEventHandler("onClientObjectBreak", root,
-    function(attacker)
-        if getElementModel( source ) == 3851 then
-            cancelEvent()
-        end
-    end
-)
+end)

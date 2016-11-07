14d172b7b017c5be80e3837182861eb22bcfc3d9
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-11-08 00:03:51 +0300

    Возможность менять цвет всех дисков сразу

--- [core]/dpGarage/client/ui/screens/ColorScreen.lua
+++ [core]/dpGarage/client/ui/screens/ColorScreen.lua
@@ -34,6 +34,8 @@ function ColorScreen:init(componentName)
 
 	self.colorPreviewEnabled = true
 	CameraManager.setState("selecting" .. componentName, false, 3)	
+
+	self.copyToAllWheels = false
 end
 
 function ColorScreen:hide()
@@ -60,6 +62,13 @@ function ColorScreen:update(deltaTime)
 			CarTexture.previewBodyColor(self.colorMenu:getColor())
 		else
 			GarageCar.previewTuning(self.componentName, {self.colorMenu:getColor()})
+			if self.copyToAllWheels then
+				if self.componentName == "WheelsColorF" then
+					GarageCar.previewTuning("WheelsColorR", {self.colorMenu:getColor()})
+				elseif self.componentName == "WheelsColorR" then
+					GarageCar.previewTuning("WheelsColorR", {self.colorMenu:getColor()})
+				end
+			end
 		end
 		--GarageCar.previewTuning(self.componentName, {self.colorMenu:getColor()})
 	end
@@ -72,6 +81,11 @@ function ColorScreen:onKey(key)
 		self.colorMenu:selectPreviousBar()
 	elseif key == "arrow_d" then
 		self.colorMenu:selectNextBar()
+	elseif key == "f" then
+		if string.find(self.componentName, "WheelsColor") then
+			self.copyToAllWheels = not self.copyToAllWheels
+			GarageCar.resetTuning()
+		end
 	elseif key == "backspace" then
 		self.colorPreviewEnabled = false
 		GarageCar.resetTuning()
@@ -82,7 +96,15 @@ function ColorScreen:onKey(key)
 		local this = self
 		Garage.buy(self.colorMenu.price, self.requiredLevel, function(success)
 			if success then
-				GarageCar.applyTuning(this.componentName, {this.colorMenu:getColor()})				
+				GarageCar.applyTuning(this.componentName, {this.colorMenu:getColor()})
+
+				if this.copyToAllWheels then
+					if this.componentName == "WheelsColorF" then
+						GarageCar.applyTuning("WheelsColorR", {this.colorMenu:getColor()})
+					elseif this.componentName == "WheelsColorR" then
+						GarageCar.applyTuning("WheelsColorR", {this.colorMenu:getColor()})
+					end
+				end						
 			end
 			CarTexture.reset()
 			this.screenManager:showScreen(ColorsScreen(self.componentName))

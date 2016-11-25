c659294546e9ee1c1ba9db5b1b30994648e6744c
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-11-25 22:03:24 +0300

    Выключена проверка кэша

--- dpStartup/client/cache_check.lua
+++ dpStartup/client/cache_check.lua
@@ -1,5 +1,5 @@
 addEventHandler("onClientResourceStart", resourceRoot, function ()
 	if not fileExists(":dpCacheLock/lock.txt") then
-		triggerServerEvent("dpCacheLock.reportMissingCache", resourceRoot)
+		--triggerServerEvent("dpCacheLock.reportMissingCache", resourceRoot)
 	end
 end)
